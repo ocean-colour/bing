5d429933e7383f30411cebeb3a82147fe8a8883e
profxj <xavier@ucolick.org>
2024-06-08 09:10:28 -0700

    MODIS + plot

--- big/plotting.py
+++ big/plotting.py
@@ -13,36 +13,53 @@ from oceancolor.hydrolight import loisel23
 from oceancolor.utils import plotting
 
 from big import chisq_fit
+from big import evaluate
 
 # ############################################################
-def show_fit(models:list, params:np.ndarray,
-                 chain_file=None, 
-                 outfile:str=None,
-                 figsize:tuple=(14,6),
-                 fontsize:float=12.,
-                 anw_true:dict=None, 
-                 bbnw_true:dict=None,
-                 Rrs_true:dict=None,
-                 show_bbnw:bool=True,
-                 log_Rrs:bool=True):
+def show_fit(models:list, inputs:np.ndarray,
+             outfile:str=None,
+             figsize:tuple=(14,6),
+             fontsize:float=12.,
+             anw_true:dict=None, 
+             bbnw_true:dict=None,
+             Rrs_true:dict=None,
+             log_Rrs:bool=True):
+    """
+    Plots the fit results for the given models and inputs.
 
+    Parameters:
+        models (list): A list of models.
+        inputs (np.ndarray): The input data for the models.
+        outfile (str, optional): The path to save the plot as an image file. Default is None.
+        figsize (tuple, optional): The size of the figure. Default is (14, 6).
+        fontsize (float, optional): The font size of the plot labels. Default is 12.0.
+        anw_true (dict, optional): The true values for `a_nw`. Default is None.
+        bbnw_true (dict, optional): The true values for `b_bnw`. Default is None.
+        Rrs_true (dict, optional): The true values for `R_rs`. Default is None.
+        show_bbnw (bool, optional): Whether to show `b_bnw` in the plot. Default is True.
+        log_Rrs (bool, optional): Whether to use a logarithmic scale for the y-axis of `R_rs`. Default is True.
+
+    Returns:
+        axes (list): A list of the axes objects used in the plot.
+    """
     # Unpack a little
     wave = models[0].wave
 
-    if params.ndim == 1:
+    if inputs.ndim == 1:
         use_LM = True
+        params = inputs
     else:
         use_LM = False
+        chains = inputs
 
     # Reconstruc
     if use_LM:
-        model_Rrs, a_mean, bb_mean = chisq_fit.fit_func(
-            wave, *params, models=models, return_full=True)
+        model_Rrs, a_mean, bb_mean = evaluate.reconstruct_chisq_fits(
+            models, params)
     else:
-        raise ValueError("Not ready for MCMC yet")
-        #a_mean, bb_mean, a_5, a_95, bb_5, bb_95,\
-        #    model_Rrs, sigRs = anly_utils.reconstruct(
-        #    models, d_chains['chains']) 
+        a_mean, bb_mean, a_5, a_95, bb_5, bb_95,\
+            model_Rrs, sigRs = evaluate.reconstruct_from_chains(
+            models, chains)
 
     # Water
     a_w = absorption.a_water(wave, data='IOCCG')
@@ -75,7 +92,7 @@ def show_fit(models:list, params:np.ndarray,
         ax_anw.plot(anw_true['wave'], anw_true['spec'], 'ko', label='True', zorder=1)
     ax_anw.plot(wave, a_mean-a_w, 'r-', label='Retreival')
     if not use_LM:
-        ax_anw.fill_between(wave, a_5-aw_interp, a_95-aw_interp, 
+        ax_anw.fill_between(wave, a_5-a_w, a_95-a_w, 
             color='r', alpha=0.5, label='Uncertainty') 
     
     ax_anw.set_ylabel(r'$a_{\rm nw}(\lambda) \; [{\rm m}^{-1}]$')
@@ -95,7 +112,7 @@ def show_fit(models:list, params:np.ndarray,
         ax_bb.plot(bbnw_true['wave'], bbnw_true['spec'], 'ko', label='True', zorder=1)
     ax_bb.plot(wave, bb_mean-bb_w, 'g-', label='Retrieval')
     if not use_LM:
-        ax_bb.fill_between(wave, bb_5-use_bbw, bb_95-use_bbw,
+        ax_bb.fill_between(wave, bb_5-bb_w, bb_95-bb_w,
             color='g', alpha=0.5, label='Uncertainty') 
 
     #ax_bb.set_xlabel('Wavelength (nm)')
