31c5422c0b5f57b53edf679ca271f8dd66c9af05
profxj <xavier@ucolick.org>
2025-08-11 05:35:32 -0700

    match and download

--- papers/biomass/Analysis/py/argo.py
+++ papers/biomass/Analysis/py/argo.py
@@ -42,6 +42,30 @@ def load_orig_argo(csv_file:str='argo_bgc_profiles_bbp.csv',
     return df
 
 def match_argo_to_pace(out_file:str, dtime:str='1 day'):
+    """
+    Matches Argo profiles to PACE granules based on spatial and temporal criteria.
+
+    Parameters:
+        out_file (str): The file path where the matched Argo profiles will be saved as a CSV.
+        dtime (str, optional): The time window for matching Argo profiles to PACE granules. 
+                                Defaults to '1 day'. The format should be compatible with 
+                                pandas.Timedelta.
+
+    Description:
+        - Loads Argo profiles and PACE granules.
+        - Checks if Argo profiles are spatially contained within PACE granules.
+        - Filters Argo profiles based on the specified time window relative to PACE granule timestamps.
+        - Matches Argo profiles to PACE granules and assigns corresponding PACE IDs to the profiles.
+        - Saves the matched Argo profiles to the specified output file.
+
+    Outputs:
+        - A CSV file containing the matched Argo profiles with additional information about 
+            the corresponding PACE granules.
+
+    Prints:
+        - The number of unique Argo profiles matched to PACE granules.
+        - The number of profiles written to the output file.
+    """
 
     # Load up Argo profiles, already cut to PACE dates
     argo_pace = load_orig_argo()
@@ -72,18 +96,31 @@ def match_argo_to_pace(out_file:str, dtime:str='1 day'):
 
     print(f'Found {np.unique(argo_idx).size} unique Argo profiles in PACE granules within {dtime}')
 
+    keep = [False]*len(argo_pace)
     # Loop on good_argo indices 
     for jj, idx in enumerate(argo_idx):
-        granule_idx = good_idx[0][jj]
+        # No need to redo
+        if keep[idx]:
+            continue
+
         # Find all matches
         ss = argo_idx == idx
+        mt_pace = np.unique(pace_idx[ss])
+        ids = ','.join(pace.iloc[mt_pace].id.values)
+        #embed(header='81 of argo')
         #
-        argo_pace.loc[idx, 'pace_id'] = pace.iloc[granule_idx].id
-        #argo_pace.loc[idx, 'pace_time'] = pace.iloc[granule_idx].time
+        argo_pace.loc[idx, 'pace_ids'] = ids
+        # Keep
+        keep[idx] = True
+
+    # Cut down
+    argo_matched = argo_pace[keep].copy()
+    # Reset index
+    argo_matched.reset_index(drop=True, inplace=True)
 
     # Write
-    argo_pace.to_csv(out_file, index=False)
-    print(f'Wrote {len(argo_pace)} profiles to {out_file}')
+    argo_matched.to_csv(out_file, index=False)
+    print(f'Wrote {len(argo_matched)} profiles to {out_file}')
 
 def scan_profiles(surface:float=20., N_surface:int=3, 
                   MLD:float=200., N_MLD:int=5,
--- papers/biomass/Analysis/py/grab_pace_granules.py
+++ papers/biomass/Analysis/py/grab_pace_granules.py
@@ -1,4 +1,8 @@
 
+import os
+import subprocess
+import numpy as np
+
 import earthaccess
 
 import pandas
@@ -6,8 +10,34 @@ import pandas
 from ocpy.utils import io as ocpy_io
 from remote_sensing.download import earthaccess as rs_ea
 
-def grab_em_all(outfile:str='PACE_50clouds.json', 
-                cloud_cover=(0,50)):
+from IPython import embed
+
+# PACE Granule path
+PACE_L2_AOP_PATH = os.path.join(os.getenv('OS_COLOR'),
+                                 'PACE',
+                                 'L2_AOP')
+
+def build_json(outfile:str='PACE_50clouds.json', cloud_cover=(0,50)):
+    """
+    Fetches granules from the PACE dataset with specified cloud cover constraints,
+    then saves the results to a JSON file.
+
+    Args:
+        outfile (str): The name of the output JSON file where the granules will be saved.
+                        Defaults to 'PACE_50clouds.json'.
+        cloud_cover (tuple): A tuple specifying the range of acceptable cloud cover percentages
+                                (min, max). Defaults to (0, 50).
+
+    Returns:
+        None
+
+    Side Effects:
+        - Authenticates the user using the `earthaccess` library.
+        - Searches for granules in the PACE dataset with the specified cloud cover range.
+        - Converts the granules to a dictionary format.
+        - Saves the dictionary as a JSON file to the specified output file.
+        - Prints the number of granules written to the output file.
+    """
 
     # Authorize
     auth = earthaccess.login(persist=True)
@@ -26,6 +56,37 @@ def grab_em_all(outfile:str='PACE_50clouds.json',
     ocpy_io.savejson(outfile, jdict)
     print(f'Wrote {len(full_dict)} granules to {outfile}')
 
+def download_matched(match_file:str):
+
+    # Load up Argo profiles, already matched to PACE
+    matched = pandas.read_csv(match_file)
+
+    # Load up PACE granules
+    granules, pace = load_from_json('PACE_50clouds.json')
+
+    # Loop on Argo profiles
+    for irow in range(len(matched)):
+        row = matched.iloc[irow]
+
+        # Get the PACE IDs
+        pace_ids = row['pace_ids'].split(',')
+
+        # Find the granules
+        for pace_id in pace_ids:
+            ss = np.where(pace.id == pace_id)[0][0]
+            granule = pace.iloc[ss]
+            #embed(header=f'Granule for {pace_id}')
+
+            outfile = os.path.join(PACE_L2_AOP_PATH, 
+                os.path.basename(granule.url))
+            # Check if already downloaded
+            if os.path.exists(outfile):
+                print(f'Already downloaded {outfile}')
+                continue
+            # wget
+            subprocess.run(['wget', '-O', outfile, granule.url])
+    print(f'Downloaded {len(matched)} Argo profiles to {PACE_L2_AOP_PATH}')        
+
 def load_from_json(json_file:str):
     # Load
     granules = ocpy_io.loadjson(json_file)
@@ -37,3 +98,18 @@ def load_from_json(json_file:str):
     # Return
     return granules, df
         
+
+# Command line
+if __name__ == '__main__':
+
+    build_json = False
+    download = True
+
+    if build_json:
+        # Build the JSON file
+        build_json(outfile='PACE_50clouds.json', cloud_cover=(0,50))
+
+    # Download nearest granules
+    if download:
+        # Download nearest granules
+        download_matched('matched_argo_bgc_profiles_bbp.csv')
