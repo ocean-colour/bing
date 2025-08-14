740f1c1ef93d3a53f2509ec9aaed475698c818f1
profxj <xavier@ucolick.org>
2025-08-14 05:43:55 -0700

    crashed

--- papers/biomass/Analysis/py/fitting.py
+++ papers/biomass/Analysis/py/fitting.py
@@ -324,8 +324,8 @@ def set_outfile(imatched:pandas.Series):
 if __name__ == '__main__':
 
     test = False
-    run_em = False
-    slurp_em = True
+    fit_em = True
+    slurp_em = False
 
     match_file = 'matched_argo_bgc_profiles_bbp.csv'
     # Load up Argo profiles, already matched to PACE
@@ -339,7 +339,7 @@ if __name__ == '__main__':
         outfile = set_outfile(imatched)
         fit_one(imatched, outfile)#, debug=True)
 
-    if run_em:
+    if fit_em:
         clobber = False
         for ss in range(len(matched)):
             imatched = matched.iloc[ss]
