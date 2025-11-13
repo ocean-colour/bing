a2211dd7774ce5025f39bd591b3fa875dc47c263
J. Xavier Prochaska <xavier@ucolick.org>
2025-11-13 11:05:02 +0900

    mo

--- bing/plotting.py
+++ bing/plotting.py
@@ -48,15 +48,10 @@ def show_fits(models:list, inputs:np.ndarray,
             or 
             chains: The MCMC chains.
         ex_a_params (np.ndarray):
-<<<<<<< HEAD
-            Extra parameters for the a model.
-        ex_bb_params (np.ndarray):
-            Extra parameters for the bb model.
-=======
+            Extra parameters for the a modegit config pull.rebase falsel.
             The extra parameters for `a_nw`, e.g. Chl
         ex_bb_params (np.ndarray):
             The extra parameters for `b_bnw`.
->>>>>>> origin/main
         outfile (str, optional): The path to save the plot as an image file. Default is None.
         figsize (tuple, optional): The size of the figure. Default is (14, 6).
         fontsize (float, optional): The font size of the plot labels. Default is 12.0.
