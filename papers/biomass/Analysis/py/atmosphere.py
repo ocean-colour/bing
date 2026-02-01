a0709a31cc1d71f8b69566e9d81ac29085438bd1
J. Xavier Prochaska <xavier@ucolick.org>
2026-01-31 19:34:54 -0800

    table for Robert

--- papers/biomass/Analysis/py/atmosphere.py
+++ papers/biomass/Analysis/py/atmosphere.py
@@ -1,9 +1,12 @@
 
 import pandas
+import numpy as np
+
+import biomass_io
 
 def table_for_robert(match_file:str, output_file:str='matched_L1B_for_robert.csv'):
     # Load up Argo profiles, already matched to PACE
-    matched = pandas.read_csv(match_file)
+    matched, _, _ = biomass_io.load_matched_data()
 
     # New table
     df = pandas.DataFrame()
@@ -19,12 +22,16 @@ def table_for_robert(match_file:str, output_file:str='matched_L1B_for_robert.csv
         L1B_files.append(l1b_file)
     df['L1B_file'] = L1B_files    
 
-    df['lat'] = matched.lat
-    df['lon'] = matched.lon
+    df['PACE_lat'] = matched.PACE_lat
+    df['PACE_lon'] = matched.PACE_lon
     df['time'] = matched.time
+    df['dist'] = matched.closest_dist_km
+
+    # Cut
+    good = np.isfinite(df.PACE_lat)
 
     # Write
-    df.to_csv(output_file, index=False)
+    df[good].to_csv(output_file, index=False)
     print(f"Wrote: {output_file}")
 
 # Command line
